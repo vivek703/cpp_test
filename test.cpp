b497b08b6b85805ffde45baa33b94afc2a8973a4
vivek703 <36715078+vivek703@users.noreply.github.com>
2018-02-21 17:21:44 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -25,7 +25,35 @@ class Encapsulation
             return x;
         }
 };
+
+// Returns characters that needs to be added
+// to make str
+string missingChars(string str)
+{
+    // A boolean array to store characters
+    // present in string.
+    bool present[MAX_CHAR] = {false};
  
+    // Traverse string and mark characters
+    // present in string.
+    for (int i=0; i<str.length(); i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            present[str[i]-'a'] = true;
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            present[str[i]-'A'] = true;
+    }
+ 
+    // Store missing characters in alphabetic
+    // order.
+    string res = "";
+    for (int i=0; i<MAX_CHAR; i++)
+        if (present[i] == false)
+            res.push_back((char)(i+'a'));
+ 
+    return res;
+}
+
 // main function
 int main()
 {
